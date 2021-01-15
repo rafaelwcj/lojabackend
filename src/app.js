962e11044a81c9a1cf74f4f06a1ffa98e46c8dc4
Rafael <rafael_cerqueira1@hotmail.com>
2021-01-15 17:35:27 -0300

    cors options

--- src/app.js
+++ src/app.js
@@ -7,19 +7,11 @@ const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-});
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.use(logger('combined'))
 app.use(bodyParser.json());
